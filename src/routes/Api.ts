9a73863a134178b7afc26cc60b951914246c6b81
Angel Angeles <daniel_1234@hotmail.es>
2022-06-03 14:47:04 -0400

    correcciones en textos de validacion

--- src/routes/Api.ts
+++ src/routes/Api.ts
@@ -13,10 +13,10 @@ const router = Router();
 
 router.post(
     '/auth/login',
-    body('username', 'E-mail cannot be blank').notEmpty(),
-    body('username', 'E-mail is not valid').isEmail(),
-    body('password', 'Password cannot be blank').notEmpty(),
-    body('password', 'Password length must be atleast 8 characters').isLength({ min: 8 }),
+    body('username', 'E-mail cannot be blank.').notEmpty(),
+    body('username', 'E-mail is not valid.').isEmail(),
+    body('password', 'Password cannot be blank.').notEmpty(),
+    body('password', 'Password length must be atleast 8 characters.').isLength({ min: 8 }),
     body('username').normalizeEmail({ gmail_remove_dots: false }),
     LoginController.perform
 );
